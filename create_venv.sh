8f14a242170e6d59cc918431c9b63649c06eb13e
Maryam Noroozizarmehri <mnorooz1@utk.edu>
2023-06-29 16:30:13 -0400

    Bugfixes to use python 2.7

--- create_venv.sh
+++ create_venv.sh
@@ -4,17 +4,16 @@ if [[ ! -d moss_phylo ]] ; then
 echo Making functional term transformers test environment [ moss_phylo ]
 echo Can install environment modules using easybuild
 module load MAFFT/7.505-GCC-11.3.0-with-extensions FastTree/2.1.11-GCCcore-11.3.0 BLAST+/2.13.0-gompi-2022a MCL/22.282-GCCcore-11.3.0 phyx/1.3-foss-2022a Cython/0.29.33-GCCcore-11.3.0 Python/2.7.18-GCCcore-11.3.0-bare
-echo Python should be from Python/3.10.4-GCCcore-11.3.0
+echo Python should be from Python/2.7
 python --version
-virtualenv --system-site-packages moss_phylo
+virtualenv moss_phylo
 
 
 source moss_phylo/bin/activate
-
 # install required pacakages
 # fastBio requires specific old versions...
 # pip3 install fastBio==0.1.7
-pip install sqlite3 networkx clint
+pip install pysqlite networkx clint
 # check packages
 pip freeze | cut -f 1 -d ' ' | column
 # accelerate==0.16.0
@@ -46,7 +45,6 @@ module load MAFFT/7.505-GCC-11.3.0-with-extensions FastTree/2.1.11-GCCcore-11.3.
 source ${PWD}/moss_phylo/bin/activate
 EOF
 cat <<"EOF" | tee -a moss_phylo_stub.srun
-export PYTHONPATH=$(python -c 'import sys; print(sys.path[-1])'):${PYTHONPATH}
 python --version
 
 EOF
